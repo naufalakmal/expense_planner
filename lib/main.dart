9e41ba69608d9f9c7c53ee27ceba418ad972703b
naufalakmal <muhammadnaufalakmal1@gmail.com>
2022-03-25 08:37:40 +0700

    More Styling

--- lib/main.dart
+++ lib/main.dart
@@ -52,19 +52,31 @@ class MyApp extends StatelessWidget {
                             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                         decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.black,
+                            color: Colors.deepOrange,
                             width: 2,
                           ),
                         ),
                         padding: EdgeInsets.all(10),
                         child: Text(
                           tx.amount.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.deepOrange,
+                          ),
                         ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.title),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(tx.date.toString(), style: TextStyle(color: Colors.grey),),
                         ],
                       )
                     ],
